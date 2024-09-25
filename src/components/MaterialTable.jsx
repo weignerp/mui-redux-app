ba051391f618bd9749f1a2409cb7f4ccbf3c242b
Pavel Weigner <weignerp@gmail.com>
2024-09-25 11:23:13 +0200

    update

--- src/components/MaterialTable.jsx
+++ src/components/MaterialTable.jsx
@@ -1,14 +1,20 @@
 import React, { useMemo, useState } from "react";
 import { MaterialReactTable, useMaterialReactTable } from "material-react-table";
-import { LinearProgress } from "@mui/material";
 import { MRT_Localization_CS } from "material-react-table/locales/cs";
-import { fakerCS_CZ as faker } from "@faker-js/faker";
-import { Avatar, Box } from "@mui/material";
-import { useGetUsersQuery } from "../features/user/userApiSlice";
+import { data } from "../data/makeData";
+import { Box } from "@mui/material";
+
+const localeStringOptions = {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+};
 
 //recommended flat structure for data, but not required (nested data is fine, but takes more setup in column definitions)
 //must be memoized or stable (useState, useMemo, defined outside of the component, etc.)
 
+/* 
 let data = [];
 for (let i = 0; i < 100; i++) {
   data.push({
@@ -20,9 +26,6 @@ for (let i = 0; i < 100; i++) {
     id: i,
   });
 }
-/*
-get max value of column amount from variable data
-*/
 let maxValue = 0;
 for (let i = 0; i < data.length; i++) {
   if (data[i].amount > maxValue) {
@@ -35,7 +38,7 @@ for (let i = 0; i < data.length; i++) {
     minValue = data[i].amount;
   }
 }
-
+ */
 /*
 const data = [
   {
@@ -50,101 +53,74 @@ const data = [
 */
 function MaterialTable() {
   const [columnFilters, setColumnFilters] = useState();
-  const columns_ = useMemo(
-    () => [
-      {
-        header: "Ticker",
-        accessorKey: "ticker", //simple recommended way to define a column
-        //more column options can be added here to enable/disable features, customize look and feel, etc.
-        //optional custom cell render
-        Cell: ({ renderedCellValue, row }) => {
-          return <Avatar sx={{ bgcolor: row.amount > 0 ? "green" : "red" }}>{renderedCellValue}</Avatar>;
-        },
-      },
-      { header: "Date", accessorKey: "date", localization: MRT_Localization_CS },
-      { header: "Description", accessorKey: "description" },
-      { header: "Amount", accessorKey: "amount" },
-      {
-        header: "Color",
-        accessorKey: "amount",
-        Cell: ({ renderedCellValue, row }) => {
-          return <LinearProgress color={row.amount > 0 ? "success" : "error"} sx={{ width: "65px", height: "10px" }} value={50} />;
-        },
-      },
-      {
-        header: "Currency",
-        accessorFn: (dataRow) => `${dataRow.amount} ${dataRow.currency}`, //alternate way to access data if processing logic is needed
-        accessorKey: "currency",
-      },
-    ],
-    []
-  );
 
   const columns = useMemo(
     () => [
-      { header: "id", accessorKey: "id", localization: "Id" },
-      { header: "name", accessorKey: "name", localization: "Jméno" },
-      { header: "username", accessorKey: "username", localization: "Uživatel" },
+      {
+        header: "First Name",
+        accessorKey: "firstName",
+      },
+      {
+        header: "Last Name",
+        accessorKey: "lastName",
+      },
+      {
+        header: "Gender",
+        accessorKey: "gender",
+      },
+      {
+        header: "State",
+        accessorKey: "state",
+      },
+      {
+        header: "Salary",
+        accessorKey: "salary",
+        aggregationFn: ["count", "mean", "median", "min", "max"],
+        //required to render an aggregated cell, show the average salary in the group
+        AggregatedCell: ({ cell }) => (
+          <>
+            Count: <Box sx={{ color: "success.main", fontWeight: "bold" }}>{cell.getValue()?.[0]}</Box>
+            Average:{" "}
+            <Box sx={{ color: "success.main", fontWeight: "bold" }}>
+              {cell.getValue()?.[1]?.toLocaleString?.("en-US", localeStringOptions)}
+            </Box>
+            Median:{" "}
+            <Box sx={{ color: "success.main", fontWeight: "bold" }}>
+              {cell.getValue()?.[2]?.toLocaleString?.("en-US", localeStringOptions)}
+            </Box>
+            Min:{" "}
+            <Box sx={{ color: "success.main", fontWeight: "bold" }}>
+              {cell.getValue()?.[3]?.toLocaleString?.("en-US", localeStringOptions)}
+            </Box>
+            Max:{" "}
+            <Box sx={{ color: "success.main", fontWeight: "bold" }}>
+              {cell.getValue()?.[4]?.toLocaleString?.("en-US", localeStringOptions)}
+            </Box>
+          </>
+        ),
+        //customize normal cell render on normal non-aggregated rows
+        Cell: ({ cell }) => <>{cell.getValue()?.toLocaleString?.("en-US", localeStringOptions)}</>,
+      },
     ],
     []
   );
 
-  const {
-    data: { data = [], meta = {} } = {}, //your data and api response will probably be different
-    isError,
-    isRefetching,
-    isLoading,
-    refetch,
-  } = useGetUsersQuery({
-    //<UserApiResponse>
-    queryKey: {
-      name: "table-data",
-      columnFilters: columnFilters || [], //refetch when columnFilters changes
-      // globalFilter, //refetch when globalFilter changes
-      // pagination.pageIndex, //refetch when pagination.pageIndex changes
-      // pagination.pageSize, //refetch when pagination.pageSize changes
-      // sorting, //refetch when sorting changes
-    },
-    /*
-    queryFn: async () => {
-      const fetchURL = new URL("/api/data", "127.0.0.1:13000");
-      console.log("fetchURL", fetchURL);
-      //read our state and pass it to the API as query params
-      // fetchURL.searchParams.set("start", `${pagination.pageIndex * pagination.pageSize}`);
-      // fetchURL.searchParams.set("size", `${pagination.pageSize}`);
-      fetchURL.searchParams.set("filters", JSON.stringify(columnFilters ?? []));
-      //fetchURL.searchParams.set("globalFilter", globalFilter ?? "");
-      // fetchURL.searchParams.set("sorting", JSON.stringify(sorting ?? []));
-
-      //use whatever fetch library you want, fetch, axios, etc
-      const response = await fetch(fetchURL.href);
-      const json = await response.json(); // as UserApiResponse;
-      return json;
-    },
-    */
-    placeholderData: true, //don't go to 0 rows when refetching or paginating to next page
-  });
   console.log("table data", data);
   //pass table options to useMaterialReactTable
   const table = useMaterialReactTable({
     columns,
-    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-    enableRowSelection: true, //enable some features
-    enableColumnOrdering: true, //enable a feature for all columns
-    enableGlobalFilter: false, //turn off a feature
-    enableDensityToggle: true, //turn off a feature
-    enableGlobalFooter: false, //turn off a feature
-    enableRowPinning: true,
-    localization: MRT_Localization_CS,
-    onColumnFiltersChange: setColumnFilters,
-    manualFiltering: true,
-
-    state: {
-      columnFilters,
-      isLoading,
-      showAlertBanner: isError,
-      showProgressBars: isRefetching,
+    data,
+    enableGrouping: true,
+    enableStickyHeader: true,
+    initialState: {
+      density: "compact",
+      expanded: true, //expand all groups by default
+      grouping: ["state"], //an array of columns to group by by default (can be multiple)
+      pagination: { pageIndex: 0, pageSize: 20 },
+      sorting: [{ id: "state", desc: false }], //sort by state by default
     },
+    muiToolbarAlertBannerChipProps: { color: "primary" },
+    muiTableContainerProps: { sx: { maxHeight: 700 } },
   });
 
   return <MaterialReactTable table={table} />;
